e4b63be3c718f84598e90597eeaca750ba13b064
Beth Carretta <bethcarretta@gmail.com>
2024-02-22 14:29:15 -0500

    modify StyledButton to accept disabled prop, added handlePress to check disabled state and styles for disabled button

--- client/src/components/StyledButton.js
+++ client/src/components/StyledButton.js
@@ -1,8 +1,19 @@
 import { Pressable, Text, StyleSheet } from 'react-native'
 
-const StyledButton = ({ onPress, title, style }) => {
+const StyledButton = ({ onPress, title, style, disabled }) => {
+  const handlePress = () => {
+    if (!disabled) {
+      onPress()
+    }
+  }
+
+  const disabledStyle = disabled ? styles.disabledBtn : {}
+
   return (
-    <Pressable style={[styles.button, style]} onPress={onPress}>
+    <Pressable
+      style={[styles.button, style, disabledStyle]}
+      onPress={handlePress}
+    >
       <Text style={styles.text}>{title}</Text>
     </Pressable>
   )
@@ -21,6 +32,9 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  disabledBtn: {
+    opacity: 0.5,
   },
 })
 
